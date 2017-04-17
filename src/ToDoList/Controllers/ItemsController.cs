52fb7ab4a4532a8284e40a242f43d813ea5332d5
Elise St Hilaire and Katy Daviscourt <elise+katy@>
2017-04-17 11:57:05 -0700

    Add full CRUD

--- src/ToDoList/Controllers/ItemsController.cs
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -51,5 +51,20 @@ namespace ToDoList.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
