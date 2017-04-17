fbe9dbf49496808c8abe9dfafdde4404510a9e33
Elise St Hilaire and Katy Daviscourt <elise+katy@>
2017-04-17 11:34:32 -0700

    begin .net todolist with add functionality

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
